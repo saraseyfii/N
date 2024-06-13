6c01b52a5da69953a7f0416b95fe4a215f1d40e5
saraseyfii <seyfisara68@gmail.com>
2024-06-13 03:48:58 +0330

    Add files via upload

---
+++ N.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+{
+	int n, temp N;
+	cout << "n1:";
+	cin >> "temp N";
+	cout << "N:";
+	cin >> n;
+	int min = tempN, max = tempN;
+	for (int i = 1; i < n, i++) {
+		cin >> tempN;
+	}
+	if (max < tempN) {
+		max = tempN;
+	}if (min>tempN) {
+		min = tempN;
+	}
+	cout << "Min" << min;
+	cout << "Max" << max;
+	return 0;
+}
